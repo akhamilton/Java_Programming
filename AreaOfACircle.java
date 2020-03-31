fe251c91b67db694b3685221fca74e29ae8deb3a
Amanda Hamilton <53063895+akhamilton@users.noreply.github.com>
2020-03-30 21:29:01 -0500

    Module 1

---
+++ AreaOfACircle.java
@@ -0,0 +1,10 @@
+public class AreaOfACircle {
+	public static void main(String[] args){
+		System.out.print("perimeter = ");
+		System.out.print(2 * 5.5 * 3.1415);
+		System.out.println();
+		System.out.print("area = ");
+		System.out.print(3.1415 * 5.5 * 5.5);
+
+	}
+}
---
+++ Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome {
+
+// display message Welcome to Java! to the console
+public static void main(String [] args) {
+
+
+System.out.println("Welcome to Java!");
+
+
+}
+
+
+
+}
