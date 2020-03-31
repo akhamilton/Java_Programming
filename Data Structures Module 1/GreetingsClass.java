9d484ee823180c12372a261e0de1853472e08aaa
Amanda Hamilton <53063895+akhamilton@users.noreply.github.com>
2020-03-30 22:05:33 -0500

    Create GreetingsClass.java

---
+++ Data Structures Module 1/GreetingsClass.java
@@ -0,0 +1,13 @@
+package com.CITC1318.course;
+
+import com.CITC1318.course.Chapters.*;
+
+public class GreetingsClass {
+    public static void main(String[] args) {
+        System.out.println("$ Greetings, CITC1318!");
+
+        Chapter1 c1 = new Chapter1();
+        Chapter2 c2 = new Chapter2();
+        Chapter3 c3 = new Chapter3();
+        }
+ }
